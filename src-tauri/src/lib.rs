793e7c3c5b8a2541ecc33bf9527fe64b66a9618d
Tyler <tyler7nguyen@duck.com>
2026-05-04 18:05:17 -0400

    feat: wire up menubar, notification plugin, and close-to-tray behavior in app setup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,8 +4,10 @@ mod platform;
 mod scanner;
 mod cleaner;
 mod commands;
+mod menubar;
 
 use commands::scan::ScanState;
+use menubar::setup_menubar;
 use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -14,6 +16,7 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_notification::init())
         .manage(ScanState::new())
         .invoke_handler(tauri::generate_handler![
             // Permission
@@ -55,6 +58,19 @@ pub fn run() {
                 let window = app.get_webview_window("main").unwrap();
                 window.open_devtools();
             }
+
+            setup_menubar(app)?;
+
+            // Close-to-tray: hide window instead of quitting
+            let window = app.get_webview_window("main").unwrap();
+            let w = window.clone();
+            window.on_window_event(move |event| {
+                if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                    api.prevent_close();
+                    let _ = w.hide();
+                }
+            });
+
             Ok(())
         })
         .run(tauri::generate_context!())
